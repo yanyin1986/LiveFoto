46cce323b3c58fa5ddcdc992d00979f7acc5bfe1
Leon.yan <yanyin1986@gmail.com>
2015-11-19 10:30:13 +0800

    first commit

---
+++ LiveFoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveFoto
+//
+//  Created by Leon.yan on 11/19/15.
+//  Copyright © 2015 Tinycomic Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
